4f8ff3646694fe2acd153d654c8554816ea936ff
nakano-shiryu <nakano.shiryu@volumetric.jp>
2025-12-16 13:36:59 +0900

    declear keyを使ってclientを作成するように変更

--- test_ocr_for_doc2/main.py
+++ test_ocr_for_doc2/main.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 from google.cloud import vision
 
+from declare_key import call_for_client
+
 p = Path(__file__).parent / 'image.png'
 
-client = vision.ImageAnnotatorClient()
-
+#client = vision.ImageAnnotatorClient()  
+client = call_for_client() # Use the imported function to get the client, this way the key file path is centralized
 with p.open('rb') as image_file:
     content = image_file.read()
 
